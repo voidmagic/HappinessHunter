78ead2ad65b3f2869dd2a12dab79da8bd278bb1e
王迁 <wangqian5730@gmail.com>
2017-11-03 17:02:33 +0800

    first commit

---
+++ app/src/main/java/com/example/wangqian/happinesshunter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.wangqian.happinesshunter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
