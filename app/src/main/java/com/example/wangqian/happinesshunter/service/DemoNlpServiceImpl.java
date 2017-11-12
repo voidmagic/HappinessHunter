e8dbc40aae57bd07d1ca9d9ebfddd59eceace1d8
王迁 <wangqian5730@gmail.com>
2017-11-12 16:23:20 +0800

    test

---
+++ app/src/main/java/com/example/wangqian/happinesshunter/service/DemoNlpServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.wangqian.happinesshunter.service;
+
+/**
+ * Created by wangqian on 2017/11/12.
+ */
+
+public class DemoNlpServiceImpl {
+}
---
+++ app/src/main/java/com/example/wangqian/happinesshunter/service/NlpService.java
@@ -0,0 +1,8 @@
+package com.example.wangqian.happinesshunter.service;
+
+/**
+ * Created by wangqian on 2017/11/12.
+ */
+
+public interface NlpService {
+}
