e2717f93a8bae49b879bd3900879f31c7db4486e
saltone <wang2erxia@163.com>
2017-11-14 10:06:14 +0800

    cool ui

--- app/src/main/java/com/example/wangqian/happinesshunter/activity/IndexActivity.java
+++ app/src/main/java/com/example/wangqian/happinesshunter/activity/IndexActivity.java
@@ -1,27 +1,183 @@
 package com.example.wangqian.happinesshunter.activity;
 
-import android.app.ActionBar;
-import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.wangqian.happinesshunter.R;
+import com.example.wangqian.happinesshunter.WordCloudActivity;
+import com.yalantis.contextmenu.lib.ContextMenuDialogFragment;
+import com.yalantis.contextmenu.lib.MenuObject;
+import com.yalantis.contextmenu.lib.MenuParams;
+import com.yalantis.contextmenu.lib.interfaces.OnMenuItemClickListener;
+import com.yalantis.contextmenu.lib.interfaces.OnMenuItemLongClickListener;
 
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Created by salto on 2017-11-13.
- */
+public class IndexActivity extends AppCompatActivity implements OnMenuItemClickListener, OnMenuItemLongClickListener {
 
-public class IndexActivity extends Activity {
+    private FragmentManager fragmentManager;
+    private ContextMenuDialogFragment mMenuDialogFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.index);
+        fragmentManager = getSupportFragmentManager();
+        initToolbar();
+        initMenuFragment();
 
-        ActionBar actionBar = getActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM); //Enable自定义的View
-            actionBar.setCustomView(R.layout.actionbar_custom);//设置自定义的布局：actionbar_custom
+    }
+
+    private void initMenuFragment() {
+        MenuParams menuParams = new MenuParams();
+        menuParams.setActionBarSize((int) getResources().getDimension(R.dimen.tool_bar_height));
+        menuParams.setMenuObjects(getMenuObjects());
+        menuParams.setClosableOutside(false);
+        mMenuDialogFragment = ContextMenuDialogFragment.newInstance(menuParams);
+        mMenuDialogFragment.setItemClickListener(this);
+        mMenuDialogFragment.setItemLongClickListener(this);
+    }
+
+    private List<MenuObject> getMenuObjects() {
+        // You can use any [resource, bitmap, drawable, color] as image:
+        // item.setResource(...)
+        // item.setBitmap(...)
+        // item.setDrawable(...)
+        // item.setColor(...)
+        // You can set image ScaleType:
+        // item.setScaleType(ScaleType.FIT_XY)
+        // You can use any [resource, drawable, color] as background:
+        // item.setBgResource(...)
+        // item.setBgDrawable(...)
+        // item.setBgColor(...)
+        // You can use any [color] as text color:
+        // item.setTextColor(...)
+        // You can set any [color] as divider color:
+        // item.setDividerColor(...)
+
+        List<MenuObject> menuObjects = new ArrayList<>();
+
+        MenuObject close = new MenuObject();
+        close.setResource(R.drawable.icn_close);
+
+        MenuObject send = new MenuObject("新的日常");
+        send.setResource(R.drawable.icn_1);
+
+        MenuObject addFav = new MenuObject("回忆云");
+        addFav.setResource(R.drawable.icn_4);
+
+        MenuObject block = new MenuObject("过往日常");
+        block.setResource(R.drawable.icn_5);
+
+//        MenuObject like = new MenuObject("Like profile");
+//        Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.icn_2);
+//        like.setBitmap(b);
+//
+//        MenuObject addFr = new MenuObject("Add to friends");
+//        BitmapDrawable bd = new BitmapDrawable(getResources(),
+//                BitmapFactory.decodeResource(getResources(), R.drawable.icn_3));
+//        addFr.setDrawable(bd);
+
+
+
+
+
+        menuObjects.add(close);
+        menuObjects.add(send);
+        menuObjects.add(addFav);
+      //  menuObjects.add(like);
+      //  menuObjects.add(addFr);
+        menuObjects.add(block);
+        return menuObjects;
+    }
+
+    private void initToolbar() {
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        TextView mToolBarTextView = (TextView) findViewById(R.id.text_view_toolbar_title);
+        setSupportActionBar(mToolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
         }
+        mToolbar.setNavigationIcon(R.drawable.btn_back);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        mToolBarTextView.setText(getResources().getString(R.string.app_name));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.context_menu:
+                if (fragmentManager.findFragmentByTag(ContextMenuDialogFragment.TAG) == null) {
+                    mMenuDialogFragment.show(fragmentManager, ContextMenuDialogFragment.TAG);
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mMenuDialogFragment != null && mMenuDialogFragment.isAdded()) {
+            mMenuDialogFragment.dismiss();
+        } else {
+            finish();
+        }
+    }
+    Intent intent;
+    @Override
+    public void onMenuItemClick(View clickedView, int position) {
+        //Toast.makeText(this, "Clicked on position: " + position, Toast.LENGTH_SHORT).show();
+        switch (position){
+            case 1:
+                intent = new Intent();
+                intent.setClass(IndexActivity.this, DiaryEditActivity.class);
+                startActivity(intent);
+                break;
+            case 2:
+                intent = new Intent();
+                intent.setClass(IndexActivity.this, WordCloudActivity.class);
+                startActivity(intent);
+                break;
+            case 3:
+                intent = new Intent();
+                intent.setClass(IndexActivity.this, DiaryActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onMenuItemLongClick(View clickedView, int position) {
+        Toast.makeText(this, "Long clicked on position: " + position, Toast.LENGTH_SHORT).show();
     }
 }
